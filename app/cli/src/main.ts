cf664a4efbf579ba642be466806a72cef6984938
thebellywolf <cdspenzer@gmail.com>
2024-05-05 15:09:34 -0400

    typescript variables update

--- app/cli/src/main.ts
+++ app/cli/src/main.ts
@@ -8,6 +8,8 @@ const poem: string = 'Roses are red, violets are blue';
 
 const numbers: number[] = [1, 2, 3];
 
+const dogNames: string[] = ['Bingo', 'Bongo', 'Bango'];
+
 let isHappy: boolean = true;
 
 isHappy = false;
@@ -16,7 +18,11 @@ let age: number = 30;
 
 age = 31;
 
-console.log(name, poem, numbers, isHappy, age);
+const person: [name: string, age: number, isHappy: boolean] = ['Mr. Mister', 31, false];
+
+const sandwich: { name: string, price: number, ingredients: string[], isPopular: boolean } = { name: 'Ham and Cheese', price: 5.99, ingredients: ['ham', 'cheese', 'bread'], isPopular: true };
+
+console.log(name, poem, numbers, dogNames[0], isHappy, age, person, sandwich.ingredients);
 
 
 
