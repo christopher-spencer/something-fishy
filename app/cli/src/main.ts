c981e347e1c892c7b31aaf8682697271121cfeca
thebellywolf <cdspenzer@gmail.com>
2024-05-05 15:01:27 -0400

    messing with typescript variables

--- app/cli/src/main.ts
+++ app/cli/src/main.ts
@@ -1,1 +1,22 @@
+/* eslint-disable @typescript-eslint/no-inferrable-types */
+
 console.log('Hello World');
+
+const name = 'Mr. Mister';
+
+const poem: string = 'Roses are red, violets are blue';
+
+const numbers: number[] = [1, 2, 3];
+
+let isHappy: boolean = true;
+
+isHappy = false;
+
+let age: number = 30;
+
+age = 31;
+
+console.log(name, poem, numbers, isHappy, age);
+
+
+
