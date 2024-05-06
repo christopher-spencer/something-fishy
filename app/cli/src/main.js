a78b91b58a8496d691294ad37944b2457c2799bc
thebellywolf <cdspenzer@gmail.com>
2024-05-06 14:38:48 -0400

    updated main.js by running tsc main.ts

--- app/cli/src/main.js
+++ app/cli/src/main.js
@@ -21,10 +21,10 @@ function eat(food) {
 console.log(add(1, 2));
 console.log(eat('pizza'));
 var subtract = function (a, b) { return a - b; };
-// const sayVehicleInfo = (vehicle: { make: string; model: string; year: number; isElectric: boolean; }): void => {
-//         console.log(`The vehicle make is ${vehicle.make}, the model is ${vehicle.model}, the year is ${vehicle.year}, and it is ${vehicle.isElectric}}`);
-// }
-// console.log(subtract(5, 3));
-// sayVehicleInfo(vehicle);
-// const addThenSubtract = (a: number, b: number, c: number): number => subtract(add(a, b), c);
-// addThenSubtract(10, 5, 3);
+var sayVehicleInfo = function (vehicle) {
+    console.log("The vehicle make is ".concat(vehicle.make, ", the model is ").concat(vehicle.model, ", the year is ").concat(vehicle.year, ", and it is ").concat(vehicle.isElectric, "}"));
+};
+console.log(subtract(5, 3));
+sayVehicleInfo(vehicle);
+var addThenSubtract = function (a, b, c) { return subtract(add(a, b), c); };
+addThenSubtract(10, 5, 3);
